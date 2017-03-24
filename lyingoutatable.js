8cf42b813e0161fc378860233c2e2565b0501594
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-24 18:37:57 +0000

    lyingoutatable.js

--- lyingoutatable.js
+++ lyingoutatable.js
@@ -1,55 +1,55 @@
-// TextCell Class
-function TextCell(text) {
-  this.text = text.split("\n");
-}
-TextCell.prototype.minWidth = function() {
-  return this.text.reduce(function(width, line) {
-    return Math.max(width, line.length);
-  }, 0);
-};
-TextCell.prototype.minHeight = function() {
-  return this.text.length;
-};
-TextCell.prototype.draw = function(width, height) {
-  var result = [];
-  for (var i = 0; i < height; i++) {
-    var line = this.text[i] || "";
-    result.push(line + repeat(" ", width - line.length));
-  }
-  return result;
-};    
-
-function TextCell(text) {
-  this.text = text.split("\n");
-}
-TextCell.prototype.minWidth = function() {
-  return this.text.reduce(function(width, line) {
-    return Math.max(width, line.length);
-  }, 0);
-};
-TextCell.prototype.minHeight = function() {
-  return this.text.length;
-};
-TextCell.prototype.draw = function(width, height) {
-  var result = [];
-  for (var i = 0; i < height; i++) {
-    var line = this.text[i] || "";
-    result.push(line + repeat(" ", width - line.length));
-  }
-  return result;
-};    
-    
-Object.defineProperty(TextCell.prototype, "heightProp", {
-  get: function() { return this.text.length; }
-});
-// End TextCell
-
 function repeat(string, times) {
   var result = "";
   for (var i = 0; i < times; i++)
     result += string;
   return result;
 }
+
+// TextCell Class
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+/*
+  minWidth() returns a number indicating this cell’s minimum width
+  (in characters).
+*/
+TextCell.prototype.minWidth = function() {
+  /* 
+    The reduce() method applies a function against an accumulator and
+    each element in the array (from left to right) to reduce it to a
+    single value.
+  */
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+/*
+  minHeight() returns a number indicating the minimum height this
+  cell requires (in lines).
+*/
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+};
+/*
+  draw(width, height) returns an array of length height, which contains
+  a series of strings that are each width characters wide. This
+  represents the content of the cell.
+*/
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};    
+
+    
+Object.defineProperty(TextCell.prototype, "heightProp", {
+  get: function() { return this.text.length; }
+});
+
+// End TextCell
 
 // UnderlinedCell Class
 function UnderlinedCell(inner) {
